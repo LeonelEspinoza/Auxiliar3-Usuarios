314772eb34ff2c939793b6a4cd77386a53759fcd
LeonelEspinoza <leonel.espinoza@ug.uchile.cl>
2023-05-13 19:37:46 -0400

    commit

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from todoapp.models import Categoria
+#from todoapp.models import Categoria
+
+#admin.site.register(Categoria)
+
+from todoapp.models import User, Tarea
+from categorias.models import Categoria
 
 admin.site.register(Categoria)
+admin.site.register(User)
+admin.site.register(Tarea)
--- todoapp/models.py
+++ todoapp/models.py
@@ -4,9 +4,15 @@ from django.db import models
 from django.utils import timezone
 
 from categorias.models import Categoria
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+  pronombres = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
+  pronombre = models.CharField(max_length=5,choices=pronombres)
+  apodo = models.CharField(max_length=30)
 
 class Tarea(models.Model):  # Todolist able name that inherits models.Model
+    owner = models.ForeignKey(User,blank=True,null=True, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=250)  # un varchar
     contenido = models.TextField(blank=True)  # un text
     fecha_creación = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))  # un date
